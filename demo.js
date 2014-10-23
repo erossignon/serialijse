c3c8b2013930634537abbbe801eb26a4d4e27f2c
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-10-24 00:10:42 +0200

    add demo.js ;  improve README

--- demo.js
+++ demo.js
@@ -1,45 +1,3 @@
-serialijse
-==========
-
-serialize and deserialize javascript object, preserve your object model
-
-
-[![Build Status](https://travis-ci.org/erossignon/serialijse.svg?branch=master)](https://travis-ci.org/erossignon/serialijse)
-[![Coverage Status](https://img.shields.io/coveralls/erossignon/serialijse.svg)](https://coveralls.io/r/erossignon/serialijse)
-[![Code Climate](https://codeclimate.com/github/erossignon/serialijse.png)](https://codeclimate.com/github/erossignon/serialijse)
-
-[![NPM](https://nodei.co/npm/serialijse.png)](https://nodei.co/npm/serialijse/)
-[![browser support](https://ci.testling.com/erossignon/serialijse.png)](https://ci.testling.com/erossignon/serialijse)
-
-
-Intro
-=====
-
-serialijse is an simple persistence framework for JavaScript that overcomes two main limitation of JSON persistence:
-
-- serialijse deals well with cyclic objects.
-- serialijse preserve object class upon deserialization.
-  
-serialijse can be used in nodejs or in the browser. It makes possible to pass data accross the network
-and recreate on the client side the same rich object model that exists on the server side.
- 
-installation
-============
-
-Using serialijse in nodejs
---------------------------
-
-````sh
-npm install serialijse
-````
-
-
-
-Examples
-========
-
-
-```javascript   
 // var s = require("serialijse");
 var s = require("./index.js");
 var assert = require("assert");
@@ -147,6 +105,5 @@ function testing_javascript_serialization_objects_with_cyclic_dependencies() {
     assert(so[0].friends[0] == so[1]); // Mary's friend is Bob
 }
 testing_javascript_serialization_objects_with_cyclic_dependencies();
-   
-   
-```
+
+
